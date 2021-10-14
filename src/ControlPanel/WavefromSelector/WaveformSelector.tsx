db6046c0e5ee0bb22e3e5d30a9f1663a340e78c2
David Franco <davidf@oz-code.com>
2021-10-14 16:37:37 +0300

    adding wave forms icons

--- src/ControlPanel/WavefromSelector/WaveformSelector.tsx
+++ src/ControlPanel/WavefromSelector/WaveformSelector.tsx
@@ -3,9 +3,10 @@ import ToggleButton from '@mui/material/ToggleButton';
 import synthAudioContext from '../../synthAudioContext';
 import { Typography } from '@mui/material';
 import StyledToggleButtonGroup from '../StyledToggleButtonGroup';
+import waveforms from "../../images/waveforms.svg";
 
 export default function WaveformSelector() {
-  const waverforms = ["sine", "square", "triangle", "sawtooth", "random"];
+  const waverforms = ["sine", "triangle", "sawtooth", "square", "random"];
   const [waveform, setWaveform] = useState("triangle");
 
   const onSelectWaveform = (selected: OscillatorType | "random") => {
@@ -29,12 +30,24 @@ export default function WaveformSelector() {
     >
       {waverforms.map((wf, index) => (
         <ToggleButton key={index} value={wf} aria-label={wf}>
-          <Typography 
-            fontSize="12px" 
-            color="var(--white-50)"
-          >
-            {wf}
-          </Typography>
+          { 
+            index === 4  
+            ? 
+            <Typography 
+              fontSize="10" 
+              color="var(--white-50)" 
+              height="20px"
+            >
+              ??
+            </Typography>
+            : 
+            <img 
+              src={`${waveforms}#svgView(viewBox(0, ${index * 4}, 6, 4))`} 
+              alt={waveform} 
+              width="30px" 
+              height="20px" 
+            />
+          }
         </ToggleButton>
       ))}
     </StyledToggleButtonGroup>
